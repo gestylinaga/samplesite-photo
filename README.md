7f83eb6b05048967bec333a68efbeff6673e8884
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-11-02 18:04:09 -0700

    project init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # samplesite-photo
 a Portfolio-Style Website Sample for a Photographer
+
+[samplesite-photo](https://gesty.dev/samplesite-photo) <- Live Site Link
+
+All "photos" from [unsplash.com](https://unsplash.com/)
+
+by: [Gesty Linaga](https://gesty.dev)
+
+
+---
+
+```javascript 
+/* TODO:
+  - fix photo grids
+*/
+```
---
+++ js/script.js
@@ -0,0 +1,15 @@
+// samplesite-photo
+console.log("by: gesty 🏄");
+
+// Intersection Observer for Section Animations
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
